215c3a9c04b7b36ae2e07527a8c989aa0d652c13
Liz Trejo <liz@liztrejo.dev>
2023-09-28 19:58:55 -0700

    fixes user password hashing to come after validation

--- models/user.js
+++ models/user.js
@@ -48,10 +48,6 @@ module.exports = (sequelize) => {
     password: {
       type: DataTypes.STRING,  
       allowNull: false,
-      set(val) {
-        const hashedPassword = bcrypt.hashSync(val, 10);
-        this.setDataValue('password', hashedPassword);
-      },
       validate: {
         notNull: {
           msg: 'A password is required'
@@ -59,6 +55,10 @@ module.exports = (sequelize) => {
         notEmpty: {
           msg: 'Please provide a password'
         },
+      },
+      set(val) {
+        const hashedPassword = val ? bcrypt.hashSync(val, 10) : "";
+        this.setDataValue('password', hashedPassword);
       }
     },
   }, { sequelize });
